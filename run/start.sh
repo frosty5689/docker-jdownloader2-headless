2b27b335c299acaf9493cabe78cce0f2e87c13c4
frosty5689 <frosty5689@gmail.com>
2018-02-09 23:25:09 -0800

    Cleanup start.sh

--- run/start.sh
+++ run/start.sh
@@ -1,37 +1,4 @@
 #!/bin/sh
 
-function stopJD2 {
-	PID=$(cat JDownloader.pid)
-	kill $PID
-	wait $PID
-	exit
-}
-
-if [ "$GID" ] && [ "$GID" -ne "0" ]
-then
-	GROUP=jdownloader
-	groupadd -g $GID $GROUP
-else
-	GROUP=root
-fi
-
-if [ "$UID" ] && [ "$UID" -ne "0" ]
-then
-	USER=jdownloader
-	useradd -r -N -s /bin/false -u $UID $USER
-else
-	USER=root
-fi
-
-#useradd -G $GROUP $USER
-#chown -R $USER:$GROUP /opt/JDownloader
-
-trap stopJD2 EXIT
-
 exec java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar
-#su -c "java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar &" -s /bin/bash $USER
-
-#while true; do
-#	sleep 3600
-#done
 
